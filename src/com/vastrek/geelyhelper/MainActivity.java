a4eca3cf3087af9118d25303d1eca222d5c5d923
eraclouds <18267109752@163.com>
2014-11-20 09:35:13 +0800

    commit

--- src/com/vastrek/geelyhelper/MainActivity.java
+++ src/com/vastrek/geelyhelper/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		setContentView(R.layout.activity_main);
 		if (savedInstanceState == null) {
 			getFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
